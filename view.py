ea12920453ec40142181665a1fe46ccf7ab55d34
Diana141004 <dianamarian001@gmail.com>
2025-12-18 12:33:55 +0200

    Learning PyQT6

--- view.py
+++ view.py
@@ -1,17 +1,50 @@
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 import sys
+from random import choice
+
+# cine_emite.semnal.connect(cine_executa)
+
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on earth',
+    'What on earth',
+    'This is surprising',
+    'This is surprising',
+    'Something went wrong'
+]
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.n_times_clicked = 0
+
         self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        self.setFixedSize(QSize(400,300))
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.windowTitleChanged.connect(self.the_window_title_changed)
 
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
 
 
 app = QApplication([])
